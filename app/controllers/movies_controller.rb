d61b8ef7a9349edc96fdd53e00ab773f753b1e32
Gabriel Apeh Adoyi <gabriel.adoyi@itcollege.ee>
2017-04-25 23:52:14 +0300

    initial commit, add first functionality

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,16 @@
+class MoviesController < ApplicationController 
+
+  # GET /movies
+  # GET /movies.json
+  def index
+    @movies = Movie.all
+     end
+
+  # GET /movies/1
+  # GET /movies/1.json
+  def show
+    
+  end
+end
+
+ 
